3c5d26c14018f9b2d835849188ef53e6fd9131ee
Tanishk Dhiman <ac94339@avcol.school.nz>
2020-07-30 15:16:15 +1200

    added modification code into modify page

--- Breakers App Revised/Modify.aspx.cs
+++ Breakers App Revised/Modify.aspx.cs
@@ -48,7 +48,7 @@ namespace Breakers_App_Revised
 
             if (sqlCon.State == ConnectionState.Closed)
                 sqlCon.Open();
-            SqlCommand sqlCmd = new SqlCommand("UserCreatOrUpdate", sqlCon);
+            SqlCommand sqlCmd = new SqlCommand("UserCreateOrUpdate", sqlCon);
             sqlCmd.CommandType = CommandType.StoredProcedure;
             sqlCmd.Parameters.AddWithValue("@UserID", (hfUserID.Value==""?0:Convert.ToInt32(hfUserID.Value)));
             sqlCmd.Parameters.AddWithValue("Username", InputUsername.Text.Trim());
@@ -59,6 +59,20 @@ namespace Breakers_App_Revised
             if (hfUserID.Value == "")
                 lblsuccessmessage.Text = "Saved Successfully";
             else lblsuccessmessage.Text = "Updated Successfully";
+
+        }
+
+        void FillGridView()
+        {
+
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("UserViewByID", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlCon.Close();
+            gvContact.DataSource = dtbl
 
         }
     }
